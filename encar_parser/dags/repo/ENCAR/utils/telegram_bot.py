bda673fb5af1fc39a18cf6e5f66a937e6d6a4241
mosin <yumimo@yandex.ru>
2024-04-29 11:38:47 +0300

    Initial commit

---
+++ encar_parser/dags/repo/ENCAR/utils/telegram_bot.py
@@ -0,0 +1,20 @@
+import logging
+from aiogram import Bot, Dispatcher
+import aiohttp
+
+
+logging.getLogger('asyncio').setLevel(logging.CRITICAL)
+# logging.basicConfig(level=logging.INFO)
+
+API_TOKEN = '6575166622:AAEtyFTwYYkLd5JQzjNcqnHZ7IVUZEM4pMs'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+session = aiohttp.ClientSession()
+
+group_chat_id = '-4051304049'
+
+
+async def send_data_to_group(search_tg_remind_id, text):
+    async with session:
+        await bot.send_message(group_chat_id, f'{text}:\n' + ' '.join(map(str, search_tg_remind_id)))
